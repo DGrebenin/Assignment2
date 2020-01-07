69cbd4a07a2380e9ab82f9f060e157d3fb13378c
Dmitry Grebenin <dgrebenin@uclan.ac.uk>
2020-01-07 20:55:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let passIn = document.getElementById("passIn");
+let usernameIn = document.getElementById("usernameIn");
+let time = 0;
+
+passIn.addEventListener("focusin", function () {
+    document.getElementById("passPopover").classList.remove("off");
+});
+
+passIn.addEventListener("focusout", function () {
+    document.getElementById("passPopover").classList.add("off");
+});
+
+usernameIn.addEventListener("focusin", function () {
+    document.getElementById("userPopover").classList.remove("off");
+});
+
+usernameIn.addEventListener("focusout", function () {
+    document.getElementById("userPopover").classList.add("off");
+});
+
+setInterval(function () {time++;}, 1);
