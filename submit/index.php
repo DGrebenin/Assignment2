18855a48194b6817111fd093d2766689bec62109
Dmitry Grebenin <dgrebenin@uclan.ac.uk>
2020-01-07 21:05:56 +0200

    shnuuf

--- submit/index.php
+++ submit/index.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Experiment</title>
+        <link href="../favicon.ico" rel="icon" type="image/ico" />
+    </head>
+    <body>
+
+    </body>
+</html>
+
 <?php echo $_POST["name"];
