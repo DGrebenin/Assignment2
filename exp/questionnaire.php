9e6af21ee387cf676ba8591b276d46d496abe1f4
Dmitry Grebenin <dgrebenin@uclan.ac.uk>
2020-01-08 14:48:16 +0200

    yes

--- exp/questionnaire.php
+++ exp/questionnaire.php
@@ -8,10 +8,13 @@
     </head>
     <body>
         <footer style="position: fixed; bottom: 0; width: 100%">
-            <div class="alert alert-green off" id="thank">
+            <div class="alert alert-green" id="thank">
                 Please accept the rules to continue!
             </div>
         </footer>
+        <script>
+            let timeElapsed = <?php echo $_POST["time"];?>;
+        </script>
         <script rel="script" type="text/javascript" src="/script.js"></script>
     </body>
 </html>
